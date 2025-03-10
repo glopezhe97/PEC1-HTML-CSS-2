c639647d569b34eb122b0b8c45b98fd964860930
glopezhe97 <gerlohe97@gmail.com>
2025-03-10 18:35:08 +0100

    continue with ingredient-list

--- src/assets/scripts/readMore.js
+++ src/assets/scripts/readMore.js
@@ -4,8 +4,10 @@ const ingredientsList = document.querySelector(".ingredients-list");
 buttonReadmore.addEventListener("click", () => {
     if (ingredientsList.style.maxHeight === '100px' || !ingredientsList.style.maxHeight) {
         ingredientsList.style.maxHeight = '1000px'; // Expande la lista (ajusta según lo necesites)
-        buttonReadmore.textContent = 'Mostrar menos'; // Cambia el texto del botón
+        buttonReadmore.textContent = 'Llegir menys...'; // Cambia el texto del botón
       } else {
         ingredientsList.style.maxHeight = '100px'; // Vuelve a ocultar los ingredientes
-        buttonReadmore.textContent = 'Mostrar más'; // Cambia el texto del botón
+        buttonReadmore.textContent = 'Llegir més...'; // Cambia el texto del botón
       }})
+
+
